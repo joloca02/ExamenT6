d0b9c7d81f843e9e7454ee544a084238f1865f98
chema <joloca02@floridauniversitaria.es>
2020-05-22 11:15:33 +0200

    terminado

--- script3.sh
+++ script3.sh
@@ -2,10 +2,9 @@ lineas=`cat balance.txt | wc -l`
 suma=0
 contador=0
 while [ $contador -le $lineas ]; do
-    linea=`cat balance.txt | head $contador`
+    valor=`cat balance.txt | head $contador | tail -1 | awk '{print $3}`
     contador=$((contador+1))
-    valor=awk '{print $3}'
-    echo "$valor"
+    suma=$((suma+linea))
     
 done
 media=$((suma/contador))
